ebc678f3a75477616f781a7916715180d235bf99
PankajAgarwal <PankajAgarwal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-13 08:42:23 +0000

    js update
    git-svn-id: https://plugins.svn.wordpress.org/formget-contact-form/trunk@771498 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/script.js
+++ js/script.js
@@ -3,18 +3,17 @@
  * and open the template in the editor.
  */
 jQuery(document).ready(function() {
-    jQuery('li#goto_fg_advance_form').click(function() {
+    jQuery('li#fg_goto_fg_advance_form').click(function() {
         jQuery('#iframebox').css({"display": "block"});
-        jQuery('div.embed_advance_form_division').css({"display": "none"});
+        jQuery('div.fg_embed_advance_form_division').css({"display": "none"});
         jQuery('.content').css({'display': 'block'});
     });
-    jQuery('li#embed_advance_form').click(function() {
-        jQuery('div.embed_advance_form_division').css({"display": "block"});
+    jQuery('li#fg_embed_advance_form').click(function() {
+        jQuery('div.fg_embed_advance_form_division').css({"display": "block"});
         jQuery('#iframebox').css({"display": "none"});
         jQuery('.content').css({'display': 'none'});
     });
     jQuery('.embed_code_save').click(function() {
-        alert("hello");
         var text_value = jQuery(this).parent().children('textarea#embed_code').val();
        var data = {
             action: 'master_response',
@@ -22,7 +21,7 @@ jQuery(document).ready(function() {
         };
         jQuery.post(embed_script_call.ajaxurl, data, function(response) {
             if (response) {
-               alert("response");
+   //alert("response");
             }
             else {
                 alert('error');
