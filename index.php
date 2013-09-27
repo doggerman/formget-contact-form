af3f8b769cf7579ffb7485adce2bb03bfdd8abe0
PankajAgarwal <PankajAgarwal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-27 12:22:34 +0000

    new tag version added
    git-svn-id: https://plugins.svn.wordpress.org/formget-contact-form/tags/1.3@779197 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,232 @@
+<?php
+/*
+  Plugin Name: FormGet Contact Form
+  Plugin URI: http://www.formget.com
+  Description: FormGet Contact Form is an eassy and effective form builder tool which enable you to bulid and embed form on your website in few steps. With FormGet Contact Form manage all your contact forms and your entire client communication at one single place.
+  Version: 1.3
+  Author: FormGet
+  Author URI: http://www.formget.com
+ */
+function my_admin_notice() {
+	$fg_iframe_form = get_option('fg_embed_code');
+	$string = "window.onload = function()";
+        $pos = strpos($fg_iframe_form, $string);
+        if ($pos == false) {
+	?>
+    <div class="trial-notify">
+        <p>
+	<a href='<?php echo admin_url('admin.php?page=cf_page'); ?>'>Click to Create your own Advance Contact Form.</a> You can add your built form to any Page, Post, Sidebar or as a Tabbed Content.</p>
+    </div>
+    <?php
+	}}
+
+add_action( 'admin_notices', 'my_admin_notice' );
+	
+
+function cf_add_style() {
+    wp_enqueue_style('form1_style1_sheet1', plugins_url('css/style.css', __FILE__));
+}
+
+add_action("init", "cf_add_style");
+//setting page
+add_action('admin_menu', 'cf_menu_page');
+
+function cf_menu_page() {
+    add_menu_page('cf', 'Contact Form', 'manage_options', 'cf_page', 'cf_setting_page', plugins_url('image/favicon.ico', __FILE__), 109);
+}
+
+function cf_setting_page() {
+    $url = plugins_url();
+    ?><div id="of_container" class="wrap">
+        <form id="ofform" action="" method="POST">
+            <div id="header">
+                <div class="logo">
+                    <h2> FormGet Contact Form</h2>
+                </div>
+                <a target="#">
+                    <div class="icon-option"> </div>
+                </a>
+                <div class="clear"></div>
+            </div>
+            <div id="main">
+
+                <div id="of-nav">
+                    <ul>
+
+                        <li> <a class="pn-view-a" href="#pn_content" title="Form Builder">Contact Form Builder </a></li>
+                        <li> <a class="pn-view-a" href="#pn_displaysetting" title="Embed Code">Embed Code</a></li>
+                        <li> <a class="pn-view-a" href="#pn_template" title="Help">Help</a></li>	
+						<li> <a class="pn-view-a" href="#pn_contactus" title="Help">Plugin Support</a></li>
+
+                    </ul>
+
+                </div>
+                <div id="content">
+                    <div class="group" id="pn_content">
+                        <h2>Contact Form Builder</h2>
+
+                        <div class="section section-text">
+                            <h3 class="heading"> Create your custom form by just clicking the fields on left side of the panel.</h3>
+
+                            <iframe src="http://www.formget.com/app" name="iframe" id="iframebox" style="width:100%; height:750px; border:1px solid #dfdfdf; align:center;">
+                            </iframe>
+                        </div>
+
+                    </div>	
+
+                    <div class="group" id="pn_displaysetting">
+                        <h2>Embed Code</h2>
+
+                        <div class="section section-text">
+                            <h3 class="heading">Paste here your tabbed code which you will get after creating form, and then click on save button. Your form will start appearing on your website.</h3>
+                            <div class="option">
+                                <div class="controls">
+                                    <textarea name="content[html]" cols="60" rows="10"   class="regular-text"  id="content_html" style="width:900px"><?php echo embeded_code(); ?></textarea>
+                                    
+									<input id="submit-form" class="embed_code_save button-primary" type="button" value="Save Changes" name="submit_form" style="display:none;">			
+									 <div id="loader_img" align="center" style="margin-left:460px; display:none;">
+                                        <img src="<?php echo plugins_url('image/ajax-loader.gif', __FILE__); ?>">
+                                    </div>
+
+                                </div>
+								
+								</div>
+                        </div>
+
+                    </div>
+                    <div class="group" id="pn_template">
+                        <h2>Steps to use FormGet Contact Form Plugin</h2>
+
+                        <div class="section section-text">
+                            <h3></h3>
+                            <div id="help_txt" style="width:900px;">
+                                <ol class="step_ol">
+
+                                    <li class="step_li">Go to "Contact Form Builder" tab and create your form by clicking on form fields.</li></br>
+                                     <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/1.png', __FILE__); ?>"></div></li></br></br></br>
+                                 <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/2.png', __FILE__); ?>"></div></li></br></br></br>
+                                    <li class="step_li">After creating your form, Register yourself through a registration popup. Your form is now ready for embedding on your site.</br></br>
+                                   <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/3.png', __FILE__); ?>"></div></li></br></br></br>
+                                    <li class="step_li">In order to add form to your website. Click the Embed link.</br></br>
+                                        <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/4.png', __FILE__); ?>"></div></li></br></br></br>
+                                    <li class="step_li">Click on Tabbed Widget and copy the whole code by clicking on "Copy Code" button.</br></br>
+                                        <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/5.png', __FILE__); ?>"></div></li></br></br></br>
+                                    <li class="step_li">Paste the copied code on "Emded Code" section in the plugin which appears on the "FormGet Contact Form" plugin dashboard inside WordPress Admin area.</br></br>
+                                        <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/6.png', __FILE__); ?>"></div></li></br></br></br>
+                                    <li class="step_li">Your contact form will start to appear on your website. You can see a Contact Us Tab appearing on the right side of all your site pages.</li></br>
+                                    <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/9.png', __FILE__); ?>"></div></li></br></br></br>
+                                    <li class="step_li">Alternatively you can also use the WordPress Shortcode tab under the Form Builder tab and use the code over there in your WordPress pages/posts to make the form appear on selective pages only.</li>
+                                    <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/8.png', __FILE__); ?>"></div></li></br></br></br>
+                     <div id="step_img" style="width:963px; border: 1px solid #dfdfdf;"><img src="<?php echo plugins_url('image/10.png', __FILE__); ?>"></div></li></br></br></br>
+    
+                                </ol><br /><br />
+                                <b><i> If you have any issues whatsoever. Email us at: neeraga@gmail.com and We will help you out with the form integration.<br /><br/>
+                                Thanks</br>
+                                FormGet Team</i></b>
+                            </div>
+                        </div>
+
+                    </div>
+					 <div class="group" id="pn_contactus">
+                      <iframe height='570' allowTransparency='true' frameborder='0' scrolling='no' style='width:100%;border:none'  src='http://www.formget.com/app/embed/form/qQvs-639'>Your Contact </iframe>
+                      
+                    </div>	
+
+                </div>
+                <div class="clear"></div>
+            </div>
+
+            <div class="save_bar_top">
+
+                
+
+            </div>
+
+        </form>
+    </div>
+
+
+    <?php
+}
+
+//embed code 
+function cf_embeded_script() {
+    wp_enqueue_script('embeded_script', plugins_url('js/script.js', __FILE__), array('jquery'));
+    wp_localize_script('embeded_script', 'script_call', array('ajaxurl' => admin_url('admin-ajax.php')));
+}
+
+add_action('init', 'cf_embeded_script');
+
+function cf_text_ajax_process_request() {
+    $text_value = $_POST['value'];
+    update_option('fg_embed_code', $text_value);
+}
+
+add_action('wp_ajax_master_response', 'cf_text_ajax_process_request');
+add_action('wp_ajax_nopriv_master_response', 'cf_text_ajax_process_request');
+if (!function_exists('embeded_code')) {
+
+    function embeded_code() {
+        global $wpdb;
+        $fg_iframe_form = get_option('fg_embed_code');
+
+        $string = "window.onload = function()";
+        $pos = strpos($fg_iframe_form, $string);
+        if ($pos == true) {
+            echo stripslashes($fg_iframe_form);
+        }
+    }
+
+}
+add_action('wp_head', 'embeded_code');
+
+//schort code function
+if (!function_exists('formget_shortcode')) {
+
+    function formget_shortcode($atts, $content = null) {
+        extract(shortcode_atts(array(
+            'user' => '',
+            'formcode' => '',
+            'allowTransparency' => true,
+            'height' => '500',
+            'tab' => ''
+                        ), $atts));
+        $iframe_formget = '';
+        $url = "http://www.formget.com/app/embed/form/" . $formcode;
+        if ($tab == 'page') {
+            $iframe_formget .="<iframe height='" . $height . "' allowTransparency='true' frameborder='0' scrolling='no' style='width:100%;border:none'  src='" . $url . "' >";
+            $iframe_formget .="</iframe>";
+            add_filter('widget_text', 'do_shortcode');
+            return $iframe_formget;
+        }
+        if ($tab == 'tabbed') {
+            $tabbed_formget = <<<EOD
+<script type="text/javascript">
+var head=document.getElementsByTagName('head')[0];
+        head.innerHTML=head.innerHTML+'<link rel="stylesheet" href="http://www.formget.com/app/app_data/user_js/dialog.css" type="text/css" />';
+        window.onload = function() {
+        var body=document.getElementsByTagName('body')[0];
+        var slide_div=document.createElement('div');
+        slide_div.setAttribute('id','slide_div');
+        slide_div.setAttribute('class','slide_div');
+        slide_div.innerHTML="<span class='pin-top'></span><span class='pin-middle'>Contact form</span><span class='pin-bottom'></span>";
+        slide_div.setAttribute('style','top:40%; right:-90px; height:40px; position:fixed;');
+        slide_div.setAttribute('onclick','document.getElementById("Formget_Dialog_Box").style.display="block"');
+        body.appendChild(slide_div);
+        var Formget_Dialog_Box=document.createElement('div');
+        Formget_Dialog_Box.setAttribute('id','Formget_Dialog_Box');
+        Formget_Dialog_Box.setAttribute('onclick','tabbed();');
+        Formget_Dialog_Box.innerHTML='<div class="formget-dialog1"></div><div class="formget-dialog2"><div class="formget-dialog3"><div class="formget-dialog4"><div class="dialog-close" title="Close Dialog"><button>Close Dialog</button></div><div class="formget-dialog5"><iframe allowtransparency="true" frameborder="0" src="{$url}"></iframe></div><a class="formget-dialog-logo" href="http://www.formget.com" target="_blank"></a></div></div></div>';
+        body.appendChild(Formget_Dialog_Box);    }   
+        function tabbed()  {
+        document.getElementById("Formget_Dialog_Box").setAttribute("style","position:fixed; top:0; left:0; width:100%; height:100%; display:none; text-align:center; z-index:2000;");
+    }
+</script>
+EOD;
+            return $tabbed_formget;
+        }
+    }
+
+}
+add_shortcode('formget', 'formget_shortcode');
+?>
---
+++ readme.txt
@@ -0,0 +1,122 @@
+=== FormGet Contact Form ===
+Contributors: PankajAgarwal
+Tags: contact us, form, contact form plugin, easy contact form, easy contact, easy contact form solution, form solution, form builder, feedback, ticket system, ticket, support system, helpdesk
+Requires at least: 3.5.0
+Tested up to: 3.5.1
+Stable tag: 1.3
+
+With FormGet Contact Form manage all your contact forms and your entire client communication at one single place.
+
+== Description ==
+
+FormGet Contact Form is an eassy and effective form builder tool which enable you to bulid and embed form on your website in few steps. Select your fields, configure your options and easily embed form on your WordPress site by embeding tabbed or shortcode, no programming knowledge is required. With FormGet Contact Form manage all your contact forms and your entire client communication at one single place while providing your customers with accountability and responsiveness they deserved.
+ 
+Full list of features:
+
+* Easy to build forms and arrange field with the help of drag and drop feature.
+* Create forms in less time and few clicks.
+* Cloud storage of data with complete security.
+* Create unlimited forms on your own dashboard.
+* Javascript/iframe codes to embed form anywhere on the site.
+* Get the form URL and share it across the network.
+* Add multiple agents.
+* Assign different queries to different agents.
+* Direct customer interaction in a gmail oriented environment.
+* Archive conversation for future reference.
+* Know click through rate ratio/stats ie; ratio of form viewers/detail submissions.
+* Analyze the form performance and focus on optimization need.
+* Change the logo icon and background color.
+* No membership charges.
+* Add unlimited number of users.
+* User likeable typography, simple and effective layout.
+
+= Demo & User Guide =
+* See demo for [Tabbed code form](http://www.formget.com/).
+* See demo for [shortcode and iframe](http://www.formget.com/contact-us/).
+* [User Guide](http://wordpress.org/plugins/formget-contact-form/screenshots/)
+
+FormGet Contact Form  is powered by formget.com.
+Click here to build [Advance Contact Form using FormGet](http://formget.com).
+
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `FormGet_Contact_Form` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Visit menu FG Contact Form to see your entries.
+
+== Screenshots ==
+
+1. Go to "Contact Form Builder" tab and create your form by clicking on form fields.
+
+2. After creating your form, Register yourself through a registration popup. Your form is now ready for embedding on your site.
+
+3. In order to add form to your website. Click the Embed link.
+
+4. Click on Tabbed Widget and copy the whole code by clicking on "Copy Code" button.
+
+5. Paste the copied code on "Emded Code" section in the plugin which appears on the "FormGet Contact Form" plugin dashboard inside WordPress Admin area. 
+
+6. Your contact form will start to appear on your website. 
+
+7. You can see a Contact Us Tab appearing on the right side of all your site pages.
+
+8. Alternatively you can also use the WordPress Shortcode tab under the Form Builder tab and use the code over there in your WordPress pages/posts to make the form appear on selective pages only.
+ 
+9. If you have any issues whatsoever. [Contact us](http://www.formget.com/contact-us/) and We will help you out with the form integration.
+
+
+== Frequently Asked Questions ==
+
+= Will FormGet Contact Form plugin work with my themes? =
+
+Of course! FormGet Contact Form plugin works out-of-the-box with every WordPress theme. It work with the vast majority of premium and free themes, without requiring any coding.
+
+= Where can I report a bug? =
+
+Report bugs and paricipate in development at http://www.formget.com/contact-us/.
+
+= How can I add contact form to my website? =
+In order to add form to your website visit the given link http://wordpress.org/plugins/formget-contact-form/screenshots/.
+
+= How can I add a field to my contact form? =
+
+To add a new field in the contact form go to your dashboard  click on edit icon of the form that you want to edit. Now you can see the section of form elements and your form. Now you can select elements and add new fields. Once the new form is created, copy the code and replace it with previous code.
+
+= Does the Contact Form Plugin save the request in database so that admin can view it? =
+
+Because we host everything, when you designe a form with FormGet it automatically builds database. you can view all your entries from your dashboard of FormGet by making yourself logged in.
+
+= How can customer log their Queries? =
+
+When you create account in FormGet you automatically get support desk. All the entries submitted from FormGet form that you have embeded on your website, get automatically converted into ticket that you can get working on, from your account. FormGet Contact Form let you collect feedback and queries directlly from your website.
+
+
+
+== Changelog ==
+
+= Version 1.3 =
+* Support tab added to handle query related to plugin
+* Admin-notice bar added to show notice until form is created
+
+= Version 1.2 =
+* Minor improvement on help section
+* Css bug fix
+
+= Version 1.1 =
+* Added shortcode for page/post
+* css improved
+* help section added
+* Bug fix: iframe
+
+= Version 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+= 1.0 =
+* Initial release
+= 1.1 =
+* Please upgrade to latest version
